18bdac400accf6c774a20035cc33ea07521b4991
Flávio Augusto Pereira <96493636+flavio-a-pereira@users.noreply.github.com>
2022-10-02 17:56:46 -0300

    update-02-10-22

--- Novo curso/Aula-POO/main.js
+++ Novo curso/Aula-POO/main.js
@@ -4,36 +4,45 @@ class Person {
       this.lastName = lastName,
       this.age = age  
     }
-
-// método para juntar first e last name
-getFullName() {
-    console.log (this.firstName + '' + this.lastName)
-}}
+}
 
 const person = new Person('Flavio', 'Pereira', 52)
 console.log(person)
 
-console.log(person.getFullName())
-
-// herança
-
-class Animal {
-    constructor(name) {
-        this.name = name
+class Person2 {
+    constructor(firstName, lastName, age){
+      this.firstName = firstName,
+      this.lastName = lastName,
+      this.age = age  
     }
-    speak() {
-        console.log(this.name  +  'latiu')
+    // método para juntar o primeiro e o último nomes
+    getfullName(){
+        console.log(this.firstName + ' ' + this.lastName)
     }
 }
+const person2 =new Person2('Joao', 'Silva', 30)
+person2.getfullName()
+
+// herança - dados de uma classe são herdados por outra
+
+class Animal {
+    constructor(name){
+        this.name = name;
+    }
+speak(){
+    console.log(this.name + ' ' + 'is cool!')
+}
+}
+const animal = new Animal('Toto')
+animal.speak()
 
 class Dog extends Animal {
     constructor(name) {
-        super(name)
+    super(name); // o super 'chama' o constructor da classe Animal
     }
+ speak(){
+    console.log(this.name + ' '+ 'barked!')
+ }   // este método sobrescreve o de cima
 }
-const animal = new Animal("Simba")
-animal.speak()
-
-const dog = new Animal('Bob')
+const dog = new Dog('Bob')
 dog.speak()
-
---
+++ Novo curso/Aula-selecao-manipulacao-elementos-DOM/main.js
@@ -0,0 +1,18 @@
+// selecionar apenas um elemento
+
+//const addUserText = document.getElementById("add-user");
+//const addUserText = document.querySelector('.container #my-form')
+
+// selecinar multiplos elementos
+
+//const allItems = document.querySelectorAll('.item')
+//const allItems = document.querySelectorAll('.items.item')
+
+// manipulação do DOM
+
+
+
+
+
+
+
